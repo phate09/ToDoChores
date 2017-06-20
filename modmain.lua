31f9ba1451f933dcfe4a1c46db9ff5304118d9b5
taichunmin <taichunmin@gmail.com>
2017-06-20 22:28:26 +0800

    modmain.lua rename GLOBAL to _G

--- modmain.lua
+++ modmain.lua
@@ -1,7 +1,9 @@
-local TUNING = GLOBAL.TUNING
---GLOBAL.CHEATS_ENABLED = true
---GLOBAL.require( 'debugkeys' )
-local OptionScreen = GLOBAL.require("widgets/options")
+local _G = GLOBAL
+local TUNING = _G.TUNING
+--_G.CHEATS_ENABLED = true
+--_G.require( 'debugkeys' )
+local OptionScreen = _G.require("widgets/options")
+local Inspect = _G.require("components/inspect")
 local widget
 
 -------------
@@ -18,31 +20,31 @@ if type(KEYBOARDTOGGLEKEY) == "string" then
 end
 --Main
 function OnActivated(player)
-  GLOBAL.ThePlayer:AddComponent("auto_chores")--add auto_chores property(?) to ThePlayer (to be accessed in chores.lua)
-  GLOBAL.ThePlayer.components.auto_chores:SetGlobal(GLOBAL)
-  GLOBAL.ThePlayer.components.auto_chores:SetEnv(env)
+  _G.ThePlayer:AddComponent("auto_chores")--add auto_chores property(?) to ThePlayer (to be accessed in chores.lua)
+  _G.ThePlayer.components.auto_chores:SetGlobal(GLOBAL)
+  _G.ThePlayer.components.auto_chores:SetEnv(env)
 end
 
 function SimPostInit(player)--after the world is initialised
   print("SimPostInit")
-  GLOBAL.TheWorld:ListenForEvent("playeractivated", OnActivated)--waits for the player to be active
+  _G.TheWorld:ListenForEvent("playeractivated", OnActivated)--waits for the player to be active
 end
 
 AddSimPostInit(SimPostInit)
 
 local function IsDefaultScreen()
-  return GLOBAL.TheFrontEnd:GetActiveScreen().name:find("HUD") ~= nil
+  return _G.TheFrontEnd:GetActiveScreen().name:find("HUD") ~= nil
 end
 local function IsMapScreen()
-  return GLOBAL.TheFrontEnd:GetActiveScreen().name:find("MapScreen") ~= nil
+  return _G.TheFrontEnd:GetActiveScreen().name:find("MapScreen") ~= nil
 end
 
 local function AddWidget(parent)
-  local ControlWidget = GLOBAL.require "widgets/chores" --load the widget chores class instance
+  local ControlWidget = _G.require "widgets/chores" --load the widget chores class instance
   widget = parent:AddChild(ControlWidget())
   widget:Hide()--widget starts hidden
 
-  GLOBAL.TheInput:AddKeyUpHandler(ToggleButton, function()--adds the eventhandler to the keyboard when pressing the button
+  _G.TheInput:AddKeyUpHandler(ToggleButton, function()--adds the eventhandler to the keyboard when pressing the button
     if not IsDefaultScreen() then return end--and not IsMapScreen()
     widget:Toggle()
   end)
@@ -57,23 +59,23 @@ AddClassPostConstruct( "widgets/controls", function (controls)
 end)
 
 local function UpdateSettings()
-  GLOBAL.ThePlayer.components.auto_chores:UpdateSettings()
+  _G.ThePlayer.components.auto_chores:UpdateSettings()
 end
 
 local function ShowOptionsScreen( controls )
 
-  if type(GLOBAL.ThePlayer) ~= "table" or type(GLOBAL.ThePlayer.HUD) ~= "table" then return end
+  if type(_G.ThePlayer) ~= "table" or type(_G.ThePlayer.HUD) ~= "table" then return end
   if not IsDefaultScreen() then return end
 
-  GLOBAL.TheFrontEnd:PushScreen(OptionScreen(controls, env))
+  _G.TheFrontEnd:PushScreen(OptionScreen(controls, env))
 end
 
 local function HideOptionsScreen(delay_focus_loss)
-  if type(GLOBAL.ThePlayer) ~= "table" or type(GLOBAL.ThePlayer.HUD) ~= "table" then return end
+  if type(_G.ThePlayer) ~= "table" or type(_G.ThePlayer.HUD) ~= "table" then return end
 
   if delay_focus_loss and controls.OptionScreen.activegesture then
     --delay a little on controllers to prevent canceling the emote by moving
-    GLOBAL.ThePlayer:DoTaskInTime(0.5, function() SetModHUDFocus("OptionScreen", false) end)
+    _G.ThePlayer:DoTaskInTime(0.5, function() SetModHUDFocus("OptionScreen", false) end)
   else
     SetModHUDFocus("OptionScreen", false)
   end
@@ -84,16 +86,16 @@ local function HideOptionsScreen(delay_focus_loss)
   if not IsDefaultScreen() then return end
 
   if controls.OptionScreen.activegesture then
-    GLOBAL.TheNet:SendSlashCmdToServer(controls.OptionScreen.activegesture, true)
+    _G.TheNet:SendSlashCmdToServer(controls.OptionScreen.activegesture, true)
   end
 end
 
 local function AddOptionsScreen( self )
   controls = self -- this just makes controls available in the rest of the modmain's functions
-  inst = GLOBAL.ThePlayer
+  inst = _G.ThePlayer
   if not handlers_applied then
     -- Keyboard controls
-    GLOBAL.TheInput:AddKeyDownHandler(KEYBOARDTOGGLEKEY, function()
+    _G.TheInput:AddKeyDownHandler(KEYBOARDTOGGLEKEY, function()
       ShowOptionsScreen(controls)
     end)
 
